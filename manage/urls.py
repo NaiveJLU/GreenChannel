dafa7776d165c269f1af0ecd387523256e3a1fce
prince19948 <wangyanjie1200@163.com>
2016-10-31 20:20:46 +0800

    add test login interface

--- manage/urls.py
+++ manage/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^record', views.index),
+    url(r'^login', views.test_login),
 ]
--- manage/views.py
+++ manage/views.py
@@ -1,8 +1,39 @@
 
 from models.Student import Student
 from django.http import HttpResponse
-
+from django.views.decorators.csrf import csrf_exempt
+import json
 def index(request):
 	stu = Student.objects.get(name='hehe')
 	return HttpResponse(str(stu.name) + str(stu.age))
 
+@csrf_exempt
+def test_login(request):
+	body = request.body
+	req = json.loads(body)
+	username = req['username']
+	password = req['password']
+	if username == 'ydc' and password == '123':
+		ret = {
+			"success": True,
+			"code": 100,
+			"message": "success",
+			"user":
+				{
+					"userId": 123,
+					"userName": "zhangsan",
+					"password": "dfajdsafsfjidsoajawie",
+					"name": "zhangsan",
+					"gender": 0,
+					"title": 1,
+					"birthday": "1994-08-23",
+				}
+		}
+		return HttpResponse(json.dumps(ret), content_type='application/json')
+
+	error = {
+		"success": False,
+		"code": 131,
+		"message": "USERNAME_NOT_EXIST",
+	}
+	return HttpResponse(json.dumps(error), content_type='application/json')
